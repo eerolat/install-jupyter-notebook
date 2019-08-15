02495cfc8561c980070ddf9b804d89b34cec3072
eerolat <46994042+eerolat@users.noreply.github.com>
2019-08-15 09:15:44 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+# Tuomas Eerola - 2019
+
+echo '==== This script will install Jupyter Notebook'
+echo '==== on your Debian 9 or Ubuntu 18.03 LTS Linux.'
+echo '==== Jupyter Notebook will be launched automatically'
+echo '==== when the installation is complete..'
+echo '===='
+
+
+sudo apt update
+sudo apt -y install python3-pip python3-dev
+sudo -H pip3 install --upgrade pip
+sudo -H pip3 install virtualenv
+mkdir ~/myprojectdir
+cd ~/myprojectdir
+virtualenv myprojectenv
+source myprojectenv/bin/activate
+pip install --user jupyter
+~/.local/bin/jupyter notebook
